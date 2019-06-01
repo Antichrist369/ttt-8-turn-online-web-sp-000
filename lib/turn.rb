3c091f122cb88a31cb23776404d5507be78894a0
Anthony Molina <antonio-molina@att.net>
2019-06-01 04:51:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,9 @@ def turn(board)
  puts "Please enter 1-9:"
  input = gets.strip
  index = input_to_index(input)
- if valid_move?(board, index)
+ if valid_move?(board, index) 
+ else  
+ end
 end
 
 # valid_move?  
